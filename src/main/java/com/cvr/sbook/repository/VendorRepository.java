23f912ea5f87b23d779a810f9681067a0bb4a091
maithri87 <srao70633@gmail.com>
2026-02-13 09:08:48 +0530

    Complete Slot Booking Backend with Auto-Slot Generation

---
+++ src/main/java/com/cvr/sbook/repository/VendorRepository.java
@@ -0,0 +1,10 @@
+package com.cvr.sbook.repository;
+
+import com.cvr.sbook.model.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+    // This interface automatically gives you Save, Find, Delete, etc.
+}
---
+++ src/test/java/com/cvr/sbook/SBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvr.sbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
